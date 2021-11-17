1614cf49f234a80ed4ff9ca3de50967459294429
Rafael Vargas <rafael.vargas@outlook.com>
2021-11-17 17:14:41 -0500

    updated header styles

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,1 +1,17 @@
 import styled from "styled-components";
+
+export const Header = styled.header`
+  header {
+    height: 320px;
+    background: url(../img/img.jpg) center no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+@media (max-width: 640px) {
+  header {
+    height: 120px;
+  }
+`;
